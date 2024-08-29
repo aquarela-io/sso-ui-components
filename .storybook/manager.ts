2190e3f111e6d2340d9d1395b07e6466d92b73a1
Diego Peixoto <contato@diegopeixoto.com>
2024-08-29 16:13:32 -0300

    Add viewport settings and dynamic title registration
    Included viewport configurations using INITIAL_VIEWPORTS and MINIMAL_VIEWPORTS, defaulting to "iphone14promax". Added dynamic document title updates to reflect current story details in Storybook.

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -1,5 +1,20 @@
 import { addons } from "@storybook/manager-api";
+import { STORY_RENDERED } from "@storybook/core-events";
 import AquarelaTheme from "./AquarelaTheme";
+
+addons.register("custom-title", (api) => {
+  const setTitle = () => {
+    const storyData = api.getCurrentStoryData();
+    const title = storyData
+      ? `${storyData.title} - ${storyData.name} ⋅ tarsila design system`
+      : "tarsila design system";
+    document.title = title;
+  };
+  setTitle();
+  api.on(STORY_RENDERED, (story) => {
+    setTitle();
+  });
+});
 
 addons.setConfig({
   theme: AquarelaTheme,
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,6 +2,10 @@ import { Preview, ReactRenderer } from "@storybook/react";
 import { withThemeByClassName } from "@storybook/addon-themes";
 import "@fontsource/inter";
 import "../src/index.css";
+import {
+  INITIAL_VIEWPORTS,
+  MINIMAL_VIEWPORTS,
+} from "@storybook/addon-viewport";
 
 const preview: Preview = {
   decorators: [
@@ -14,10 +18,16 @@ const preview: Preview = {
     }),
   ],
   tags: ["autodocs"],
-
   parameters: {
     docs: {
       toc: true,
+    },
+    viewport: {
+      viewports: {
+        ...INITIAL_VIEWPORTS,
+        ...MINIMAL_VIEWPORTS,
+      },
+      defaultViewport: "iphone14promax",
     },
     controls: {
       matchers: {
