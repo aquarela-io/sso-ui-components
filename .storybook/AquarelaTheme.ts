41203226245c5ec28735c8d1312d1f156f1ce05a
Diego Peixoto <contato@diegopeixoto.com>
2024-08-29 10:34:52 -0300

    Add Storybook theme customization and Vercel deployment settings
    Introduced a custom dark theme for Storybook with specific branding for Aquarela. Configured Vercel to deploy Storybook with custom commands and output directory. Updated .gitignore to exclude the generated Storybook static files.

---
+++ .storybook/AquarelaTheme.ts
@@ -0,0 +1,9 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+  base: "dark",
+  brandTitle: "aquarela SSO UI components",
+  brandUrl: "https://aquarela.io",
+  brandImage: "https://static.aquarela.uk/_images/aquarela-logo-site.svg",
+  brandTarget: "_self",
+});
---
+++ .storybook/manager.ts
@@ -0,0 +1,6 @@
+import { addons } from "@storybook/manager-api";
+import AquarelaTheme from "./AquarelaTheme";
+
+addons.setConfig({
+  theme: AquarelaTheme,
+});
